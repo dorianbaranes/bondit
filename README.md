9dc51a41f8bac826d93e2f5db9e5fe4c01ddda11
dorianbaranes <85120433+dorianbaranes@users.noreply.github.com>
2022-07-28 23:35:18 +0300

    add API

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 
 ## Test BondIT
 
-#### <br/>Requirements 
+### <br/>Requirements 
 
 [PyCharm's requirements management tool](https://www.jetbrains.com/help/pycharm/managing-dependencies.html#create-requirements)
 is used to sync Python requirements.
 
-#### <br/>Launch API
+You can use also:
+` pip install -r requirement.txt`
+
+
+
+### <br/>Launch API
 
 Command: <br/>
 `uvicorn main:app --reload`
 
-And then go to : http://127.0.0.1:8000/docs
+And to see API doc go to : http://127.0.0.1:8000/docs
+
+### <br/>Part 1: Write down code that produce the success column
+Call the API endpoint: <br/>
+`http://127.0.0.1:8000/set_flight_status`
+
+### <br/>Part 2.1: Get to get info about flight
+Call the API endpoint: <br/>
+`http://127.0.0.1:8000/get_all_flights`
+
+### <br/>Part 2.2: Update a flight
+Call the API endpoint: <br/>
+`http://127.0.0.1:8000/update_flight`
--- main.py
+++ main.py
@@ -3,48 +3,7 @@ from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
-###################################################PART 1
-def analyze_flights(csv_filename):
-    flight_per_arrival_time = dict()
-    arrival_times = set()
 
-    with open(csv_filename, 'r') as f:
-        csv_reader = csv.reader(f, delimiter=',')
-        next(csv_reader)
-
-        for row in csv_reader:
-            arrival_times.add(row[2])
-
-            if row[2] in flight_per_arrival_time.keys():
-                flight_per_arrival_time[row[2]].append(row)
-            else:
-                flight_per_arrival_time[row[2]] = [row]
-
-    arrival_times = list(arrival_times)
-    arrival_times.sort()
-
-    with open(csv_filename, 'w', newline='') as f2:
-        csv_writer = csv.writer(f2)
-        nb_success = 0
-        for arrival_time in arrival_times:
-            flights_per_arrival_time = flight_per_arrival_time[arrival_time]
-
-            for flight in flights_per_arrival_time:
-                depart = datetime.strptime(flight[1].strip(), '%H:%M')
-                arrival = datetime.strptime(flight[2].strip(), '%H:%M')
-                delta = arrival - depart
-                delta = int(delta.total_seconds() / 60)
-
-                if delta >= 180 and nb_success <= 20:
-                    flight[3] = 'success'
-                    nb_success+=1
-                else:
-                    flight[3] = 'fail'
-
-                csv_writer.writerow(flight)
-
-
-##################################################PART 2
 class Flight(BaseModel):
     id : str
     departure: str
@@ -69,11 +28,8 @@ async def get_all_flights():
     return all_flights
 
 
-@app.post("/update_flight")
+@app.put("/update_flight")
 async def update_flight(flight:Flight):
-    print(flight.dict())
-
-
     df = pd.read_csv(csv_filename, sep=',')
     df=df.set_index('id')
 
@@ -91,4 +47,48 @@ async def update_flight(flight:Flight):
         return {'message':'This flight has been updated'}
     else:
         return {'message':'This flight does not exist'}
+
+
+@app.put("/set_flight_status")
+def set_flight_status():
+    flight_per_arrival_time = dict()
+    arrival_times = set()
+
+    with open(csv_filename, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        header=next(csv_reader)
+
+        for row in csv_reader:
+            arrival_times.add(row[2])
+
+            if row[2] in flight_per_arrival_time.keys():
+                flight_per_arrival_time[row[2]].append(row)
+            else:
+                flight_per_arrival_time[row[2]] = [row]
+
+    arrival_times = list(arrival_times)
+    arrival_times.sort()
+
+    with open(csv_filename, 'w', newline='') as f2:
+        csv_writer = csv.writer(f2)
+        csv_writer.writerow(header)
+        nb_success = 0
+        for arrival_time in arrival_times:
+            flights_per_arrival_time = flight_per_arrival_time[arrival_time]
+
+            for flight in flights_per_arrival_time:
+                depart = datetime.strptime(flight[1].strip(), '%H:%M')
+                arrival = datetime.strptime(flight[2].strip(), '%H:%M')
+                delta = arrival - depart
+                delta = int(delta.total_seconds() / 60)
+
+                if delta >= 180 and nb_success <= 20:
+                    flight[3] = 'success'
+                    nb_success+=1
+                else:
+                    flight[3] = 'fail'
+
+                csv_writer.writerow(flight)
+
+    return {'message:Status updated'}
 
