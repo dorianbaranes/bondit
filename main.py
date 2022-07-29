b13a90b3b1de4856f58ba6bc88930a596e42a747
dorianbaranes <85120433+dorianbaranes@users.noreply.github.com>
2022-07-29 12:10:47 +0300

    Update with new API endpoint

--- main.py
+++ main.py
@@ -14,26 +14,34 @@ class Flight(BaseModel):
 app = FastAPI()
 csv_filename='flight.csv'
 
-@app.get("/get_all_flights")
+@app.get("/all_flights")
 async def get_all_flights():
+    df = pd.read_csv(csv_filename, sep=',')
+    df = df.set_index('id', drop=False)
 
-    with open(csv_filename, 'r') as f:
-        csv_reader = csv.reader(f, delimiter=',')
-        next(csv_reader)
-
-        all_flights=list()
-        for row in csv_reader:
-            all_flights.append(row)
+    all_flights = list()
+    for index,row in df.iterrows():
+        print(type(row))
+        all_flights.append(row)
 
     return all_flights
 
+@app.get("/flight/{flight_id}")
+async def get_flight(flight_id):
+
+    df = pd.read_csv(csv_filename, sep=',')
+    df = df.set_index('id', drop=False)
+
+    if flight_id in df.index:
+        return df.loc[flight_id].to_dict()
+    else:
+        return {'message':'This flight does not exist'}
 
 @app.put("/update_flight")
 async def update_flight(flight:Flight):
     df = pd.read_csv(csv_filename, sep=',')
     df=df.set_index('id')
 
-    #update
     if flight.id in df.index:
         try:
             datetime.strptime(flight.departure.strip(), '%H:%M')
@@ -44,6 +52,7 @@ async def update_flight(flight:Flight):
 
         df.loc[flight.id] = flight.departure, flight.arrival, flight.status
         df.to_csv(csv_filename, index=True)
+        set_flight_status()
         return {'message':'This flight has been updated'}
     else:
         return {'message':'This flight does not exist'}
@@ -52,21 +61,18 @@ async def update_flight(flight:Flight):
 @app.put("/set_flight_status")
 def set_flight_status():
     flight_per_arrival_time = dict()
-    arrival_times = set()
 
     with open(csv_filename, 'r') as f:
         csv_reader = csv.reader(f, delimiter=',')
         header=next(csv_reader)
 
         for row in csv_reader:
-            arrival_times.add(row[2])
-
-            if row[2] in flight_per_arrival_time.keys():
-                flight_per_arrival_time[row[2]].append(row)
+            if row[2].strip() in flight_per_arrival_time.keys():
+                flight_per_arrival_time[row[2].strip()].append(row)
             else:
-                flight_per_arrival_time[row[2]] = [row]
+                flight_per_arrival_time[row[2].strip()] = [row]
 
-    arrival_times = list(arrival_times)
+    arrival_times = list(flight_per_arrival_time.keys())
     arrival_times.sort()
 
     with open(csv_filename, 'w', newline='') as f2:
